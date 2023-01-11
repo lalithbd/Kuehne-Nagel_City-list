392cc6fea6cbc1ac91b12b513f961d54868e232b
lalithbuddhika <lalithbuddhika16@gmail.com>
2023-01-11 06:16:01 +0530

    Constant for name,%

---
+++ city-list_back-end/src/main/java/KuehneNagel/Citylist/util/Constant.java
@@ -0,0 +1,7 @@
+package KuehneNagel.Citylist.util;
+
+public class Constant {
+
+    public static final String PERCENTAGE_SIGN = "%";
+    public static final String SEARCH_COLUMN_NAME = "name";
+}
