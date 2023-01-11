7d67599b7ce8965fd6650991c632d20a9cb10866
lalithbuddhika <lalithbuddhika16@gmail.com>
2023-01-11 06:22:42 +0530

    missing @service annotation

--- city-list_back-end/src/main/java/KuehneNagel/Citylist/service/external/implementation/HttpServiceImpl.java
+++ city-list_back-end/src/main/java/KuehneNagel/Citylist/service/external/implementation/HttpServiceImpl.java
@@ -3,9 +3,11 @@ package KuehneNagel.Citylist.service.external.implementation;
 import KuehneNagel.Citylist.service.external.HttpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+@Service
 public class HttpServiceImpl implements HttpService {
 
     @Autowired
