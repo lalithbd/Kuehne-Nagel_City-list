04d1e8c7185e296388ee9ba870d85b8f49c3e068
lalithbuddhika <lalithbuddhika16@gmail.com>
2023-01-15 23:55:02 +0530

    Cors config updated for swagger

--- city-list_back-end/src/main/java/KuehneNagel/Citylist/configuration/CORSConfiguration.java
+++ city-list_back-end/src/main/java/KuehneNagel/Citylist/configuration/CORSConfiguration.java
@@ -2,11 +2,9 @@ package KuehneNagel.Citylist.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class CORSConfiguration implements WebMvcConfigurer {
 
     @Override
